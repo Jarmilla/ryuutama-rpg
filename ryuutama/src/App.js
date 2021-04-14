4383ccdcb19d23659f92e7cb3b8c460f6a1a46b2
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-04-14 11:09:04 +0200

    initial react app

---
+++ ryuutama/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div>
+          <h1>Ryuutama</h1>
+          <h2>Charcter Sheet</h2>
+        </div>
+        <div></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
