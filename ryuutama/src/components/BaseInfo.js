ba63f5ee3c2ff33f1d9539f41cc71f73c4f10c66
Jarmilla <bodorne.deme.adrienn@gmail.com>
2022-02-21 15:22:26 +0100

    easier readability to BaseInfo

--- ryuutama/src/components/BaseInfo.js
+++ ryuutama/src/components/BaseInfo.js
@@ -29,6 +29,7 @@ function BaseInfo({
   return (
     <div className="character-sheet">
       <div className="character-rows">
+
         <div className="input-area">
           <h2> Character name: </h2>
           {isSaved ? <p>{charName}</p> : <input type="text" value={charName} required onChange={passTargetValueTo(setCharName)} />}
@@ -41,12 +42,15 @@ function BaseInfo({
       </div>
 
       <div className="character-rows">
+
         <div className="input-area">
           <h2>Level:</h2>
           <p>{levelUpChar(exp)}</p>
+
           <h2>EXP: </h2>
           {isSaved ? <p>{exp}</p> : <input type="number" value={exp} required onChange={passTargetValueTo(setExp)} />}
         </div>
+
         <div className="input-area">
           <h2>Sex: </h2>
           {isSaved ? (
@@ -58,30 +62,37 @@ function BaseInfo({
               <option value="other">Other</option>
             </select>
           )}
+
           <h2>Age: </h2>
           {isSaved ? <p>{age}</p> : <input type="number" value={age} required onChange={passTargetValueTo(setAge)} />}
         </div>
       </div>
 
       <div className="character-rows">
+
         <div className="input-area">
           <h2>Class:</h2>
           {isSaved ? <p>{class1}</p> : <ClassChooser setClass={setClass1} />}
           <p> / </p>
-          { exp >= 2000 ? <ClassChooser setClass={setClass2} /> : <p>2nd Class</p>  }
-       
+          
+          { exp >= 2000 ? 
+          isSaved ? <p>{class2}</p> : <ClassChooser setClass={setClass2} /> 
+          : <p>2nd Class</p>  }
         </div>
+
         <div className="input-area">
           <h2>Type:</h2>
           {isSaved ? <p>{type1}</p> : <TypeChooser setType={setType1} />}
           <p> / </p>
-          { exp >= 3000 ? <TypeChooser setType={setType2} /> :  <p>2nd Type</p>  }
+
+          { exp >= 3000 ? 
+            isSaved ? <p>{type2}</p> : <TypeChooser setType={setType2} /> 
+            :  <p>2nd Type</p>  }
         </div>
       </div>
 
       <TypeInfoDisplayer type={type1} />
-      { exp >= 3000 ? <TypeInfoDisplayer type={type2} /> : ""  }
-      {/*  ha elért x levelre Show <TypeInfoDisplayer type={type2} /> */}
+      { exp >= 3000 ? <TypeInfoDisplayer type={type2} /> : "" }
     </div>
   );
 }
